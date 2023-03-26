4e58d3403cca2170a9301166648b17591e357737
Javier Vargas - Legion <javieravargasestudiante@gmail.com>
2023-03-26 14:05:43 -0500

    Boceto inicial mobile

---
+++ src/components/BasseApparel.jsx
@@ -0,0 +1,27 @@
+
+export const BasseApparel = () => {
+    return (
+        <section className="section-main">
+            <div className="img-logo">
+                <img src="public/images/logo.svg" alt="Logo" />
+            </div>
+            <div className="img-heroe">
+                <img src="public/images/hero-mobile.jpg" alt="Héroe mobile" />
+            </div>
+            <article className="base-form">
+                <p className="title-base">
+                    WE'RE <span>COMING SOON</span>
+                </p>
+                <p className="paragraph-form">
+                    Hellow fellow shappers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launc deals.
+                </p>
+                <div className="form-base">
+                    <input type="email" name="email" id="email-form" placeholder="Email Address."/>
+                    <div className="image-form">
+                        <img src="public/images/icon-arrow.svg" alt="" />
+                    </div>
+                </div>
+            </article>
+        </section>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BasseApparel } from './components/BasseApparel'
+import './css/styles-baseapparel.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BasseApparel />
+    </React.StrictMode>,
+)
